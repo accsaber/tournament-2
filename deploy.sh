2221d0d6f09b653d43393416ddd42d59cfca8447
George Fischer <me@george.id.au>
2024-08-26 13:40:08 +1000

    deploy tweaks

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,25 @@ export IMAGE_TAG=latest
 
 export IMAGE=$IMAGE_NAME:$IMAGE_TAG
 
-echo "building image..."
+echo -e "\x1b[1mbuilding image...\x1b[0m"
 docker build . \
     --platform=linux/arm64 \
     -t $IMAGE --push \
     || exit $?
-export IMAGE=$(docker inspect --format='{{index .RepoDigests 0}}' $IMAGE)
+export IMAGE=$(docker inspect --format='{{index .RepoDigests 0}}' $IMAGE | tr -d '\n')
 export DIGEST=${IMAGE#*@}
-echo -ne "deploying \x1b[32m"
-echo $DIGEST
+echo -ne "\x1b[1mdeploying \x1b[32m"
+echo -n $DIGEST
 echo -e "\x1b[0m"
-cp manifests/kustomization.yml manifests/_kustomization.yml
-yq -i ".images[0].digest = \"$DIGEST\"" manifests/kustomization.yml || exit $?
-yq -i ".images[0].newName = \"$IMAGE_NAME\"" manifests/kustomization.yml || exit $?
-yq -i ".images[0].newTag = \"$IMAGE_TAG\"" manifests/kustomization.yml || exit $?
-kubectl apply -k manifests --server-side --context accsaber || exit $?
-mv manifests/_kustomization.yml manifests/kustomization.yml
+
+export MANIFESTS="/tmp/manifests-$(tr -d '\n' <<< $DIGEST | tail -c7)"
+echo -ne "patched manifests copied to \x1b[33m"
+echo -n $MANIFESTS
+echo -e "\x1b[0m"
+
+cp -r manifests $MANIFESTS
+yq -i ".images[0].digest = \"$DIGEST\"" $MANIFESTS/kustomization.yml || exit $?
+yq -i ".images[0].newName = \"$IMAGE_NAME\"" $MANIFESTS/kustomization.yml || exit $?
+yq -i ".images[0].newTag = \"$IMAGE_TAG\"" $MANIFESTS/kustomization.yml || exit $?
+kubectl apply -k $MANIFESTS --server-side --context accsaber || exit $?
+rm -r $MANIFESTS
--- load_secret.fish
+++ load_secret.fish
@@ -1,3 +1,3 @@
-export BEATLEADER_CLIENT_SECRET=(kubectl get secret beatleader-oauth -o jsonpath="{.data.client_secret}" | base64 -d)
-export DISCORD_CLIENT_ID=(kubectl get secret discord-oauth -o jsonpath="{.data.client_id}" | base64 -d)
-export DISCORD_CLIENT_SECRET=(kubectl get secret discord-oauth -o jsonpath="{.data.client_secret}" | base64 -d)
+export BEATLEADER_CLIENT_SECRET=(kubectl get secret beatleader-oauth --context accsaber -o jsonpath="{.data.client_secret}" | base64 -d)
+export DISCORD_CLIENT_ID=(kubectl get secret discord-oauth --context accsaber -o jsonpath="{.data.client_id}" | base64 -d)
+export DISCORD_CLIENT_SECRET=(kubectl get secret discord-oauth --context accsaber -o jsonpath="{.data.client_secret}" | base64 -d)echo
