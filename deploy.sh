ef5874271425b34115849e19971eff9d481895ea
George Fischer <me@george.id.au>
2024-08-29 14:13:41 +1000

    parallelise deploy

--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,17 @@ cp -r manifests $MANIFESTS
 yq -i ".images[0].digest = \"$DIGEST\"" $MANIFESTS/kustomization.yml || exit $?
 yq -i ".images[0].newName = \"$IMAGE_NAME\"" $MANIFESTS/kustomization.yml || exit $?
 yq -i ".images[0].newTag = \"$IMAGE_TAG\"" $MANIFESTS/kustomization.yml || exit $?
-kubectl apply -k $MANIFESTS --server-side --context accsaber || exit $?
+mkdir -p $MANIFESTS/built
+kubectl kustomize $MANIFESTS > $MANIFESTS/all.yml
+gcsplit --elide-empty-files  --quiet -f $MANIFESTS/built/chunk $MANIFESTS/all.yml "/---/" "{*}"
+pids=()
+for chunk in $MANIFESTS/built/chunk*; do
+    kubectl apply -f $chunk --context accsaber --server-side &
+    pids[${chunk//[^0-9]/}]=$!
+done
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+echo -e "\x1b[1mcleaning up\x1b[0m"
 rm -r $MANIFESTS
