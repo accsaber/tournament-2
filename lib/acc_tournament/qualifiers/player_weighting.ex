d43e7687026e6088ae14c0b33a96165287d07351
George Fischer <me@george.id.au>
2024-09-03 21:45:15 +1000

    fix incredibly stupid player weighting issue

--- lib/acc_tournament/qualifiers/player_weighting.ex
+++ lib/acc_tournament/qualifiers/player_weighting.ex
@@ -5,19 +5,20 @@ defmodule AccTournament.Qualifiers.PlayerWeighting do
   use Oban.Worker
   import Ecto.Query, only: [from: 2, subquery: 1]
 
+  @counted_attempts 3
+
   @impl Oban.Worker
   def perform(%Oban.Job{}) do
     attempts =
       from(a in Attempt,
         distinct: [a.map_id, a.player_id],
-        where: not is_nil(a.score),
         order_by: [asc: a.map_id, asc: a.player_id, desc: a.score]
       )
 
     attempts =
       from(a in subquery(attempts),
         order_by: [desc: a.score],
-        select: {dense_rank() |> over(partition_by: a.map_id, order_by: [desc: a.score]), a},
+        where: not is_nil(a.weight),
         preload: [:player]
       )
 
@@ -32,24 +33,19 @@ defmodule AccTournament.Qualifiers.PlayerWeighting do
         id = player.id
 
         attempts =
-          for {_, %Attempt{player_id: ^id} = attempt} <- r.attempts, do: attempt
+          for %Attempt{player_id: ^id} = attempt <- r.attempts, do: attempt
 
         weights = for attempt <- attempts, do: attempt.weight
 
-        n = length(weights)
+        padding =
+          Stream.cycle([25])
+          |> Enum.take(@counted_attempts)
 
         weights =
-          if n < 3 do
-            padding =
-              Stream.cycle([25])
-              |> Enum.take(3 - n)
+          (weights ++ padding)
+          |> Enum.take(@counted_attempts)
 
-            weights ++ padding
-          else
-            weights
-          end
-
-        average_weight = Enum.reduce(weights, &(&1 + &2)) / 3
+        average_weight = Enum.reduce(weights, &(&1 + &2)) / @counted_attempts
 
         User.average_weight_changeset(player, %{average_weight: average_weight})
         |> Repo.update!()
