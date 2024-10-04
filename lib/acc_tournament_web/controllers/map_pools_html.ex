b589d684a3d9907461df1aee063b18bc2c3e03a3
George Fischer <me@george.id.au>
2024-10-04 10:24:29 +1000

    disable pool categories

--- lib/acc_tournament_web/controllers/map_pools_html.ex
+++ lib/acc_tournament_web/controllers/map_pools_html.ex
@@ -16,7 +16,7 @@ defmodule AccTournamentWeb.MapPoolsHTML do
 
   def listing(%{pools: _} = assigns) do
     ~H"""
-    <div class="flex flex-col bg-padding-bg gap-10 rounded p-4 prose max-w-none dark:prose-invert">
+    <div class="flex flex-col gap-4 bg-padding-bg gap-10 rounded p-4 prose max-w-none dark:prose-invert">
       <div :for={pool <- @pools} class="flex items-center gap-4">
         <div class="text-lg">
           <%= pool.name %>
@@ -55,82 +55,30 @@ defmodule AccTournamentWeb.MapPoolsHTML do
         </a>
       </:subtitle>
     </.header>
-    <div :if={@pool.name == "Qualifiers"} class="prose max-w-none dark:prose-invert">
-      <div class="bg-padding-bg rounded px-4 py-3 pb-1">
-        <table class="my-0">
-          <thead>
-            <tr>
-              <th>Song Name</th>
-              <th>Artist</th>
-              <th>Mapper</th>
-              <th>Difficulty</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr :for={map <- @maps}>
-              <td><%= map.name %></td>
-              <td><%= map.artist %></td>
-              <td><%= map.mapper %></td>
-              <td><%= diff_name(map.difficulty) %></td>
-              <td>
-                <a href={"https://beatsaver.com/maps/#{map.beatsaver_id}"}>
-                  !bsr <%= map.beatsaver_id %>
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <div
-      :if={@pool.name != "Qualifiers"}
-      class="flex flex-col space-y-4 prose max-w-none dark:prose-invert"
-    >
-      <div :for={category <- @categories} class="bg-padding-bg rounded px-4 py-3 pb-1">
-        <.header class="inline-block">
-          <%= category.name %>
-          <:subtitle>
-            <a
-              href={~p"/api/map_pools/#{@pool.id}/#{category.name}/playlist"}
-              download={"acc-#{filename(@pool.name)}-#{filename(category.name)}.bplist"}
-              class={[
-                "rounded bg-neutral-800 hover:bg-neutral-700 py-1.5 px-3",
-                "text-sm font-semibold leading-6 dark:text-white flex gap-2 items-center w-fit"
-              ]}
-            >
-              <.icon name="hero-arrow-down-tray-mini" /> Download Playlist
-            </a>
-          </:subtitle>
-        </.header>
-        <table class="my-0">
-          <thead>
-            <tr>
-              <th>Song Name</th>
-              <th>Artist</th>
-              <th>Mapper</th>
-              <th>Difficulty</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr :for={map <- Enum.filter(@maps, &(&1.category.id == category.id))}>
-              <td><%= map.name %></td>
-              <td><%= map.artist %></td>
-              <td><%= map.mapper %></td>
-              <td>
-                <%= diff_name(map.difficulty) %>
-                <%= if map.map_type == "Lawless" do %>
-                  (L)
-                <% end %>
-              </td>
-              <td>
-                <a href={"https://beatsaver.com/maps/#{map.beatsaver_id}"}>
-                  !bsr <%= map.beatsaver_id %>
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div class="prose max-w-none dark:prose-invert mt-4">
+      <table>
+        <thead>
+          <tr>
+            <th>Song Name</th>
+            <th>Artist</th>
+            <th>Mapper</th>
+            <th>Difficulty</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr :for={map <- @maps}>
+            <td><%= map.name %></td>
+            <td><%= map.artist %></td>
+            <td><%= map.mapper %></td>
+            <td><%= diff_name(map.difficulty) %></td>
+            <td>
+              <a href={"https://beatsaver.com/maps/#{map.beatsaver_id}"}>
+                !bsr <%= map.beatsaver_id %>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     """
   end
